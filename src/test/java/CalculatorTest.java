0dab283231c6f6bbfb257354268daadc0eb8f3d6
Mikaela-D <G00401392@atu.ie>
2023-05-19 20:47:32 +0100

    Added code to Calculator class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,7 @@
-import ie.atu.Calculator;
-
 public class CalculatorTest {
     private int nbErrors = 0;
-
     public static void main(String[] args) {
         CalculatorTest test = new CalculatorTest();
-
         try {
             test.testAdd();
         }
