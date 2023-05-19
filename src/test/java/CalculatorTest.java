0216fc8d5cda6497c8795838a4326d0c7028136b
Mikaela-D <G00401392@atu.ie>
2023-05-19 20:28:11 +0100

    Added a try catch

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,5 +4,13 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         CalculatorTest test = new CalculatorTest();
+
+        try {
+            test.testAdd();
+        }
+        catch (Throwable e) {
+            test.nbErrors++;
+            e.printStackTrace();
+        }
     }
 }
